f68b5aa158a08813a62f47e713c11aeaced06b71
Lukas Akdogan <lukas.akdogan@gmail.com>
2024-03-25 21:37:39 +0100

    Create app.py
    Signed-off-by: Lukas Akdogan <lukas.akdogan@gmail.com>

---
+++ projects/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'your_strong_secret_key' 
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(120))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Get form data...
+        # Validate
+        # Hash password
+        user = User(username=username, password_hash=password_hash)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+# ... (login and logout routes below)
+
+if __name__ == '__main__':
+    db.create_all()  # Create the database schema
+    app.run(debug=True)
